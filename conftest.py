7dd2e56f9e05cb04edd0fdb103a0e2d6887ba525
Taras Vederko <vederkoat@gmail.com>
2025-04-12 14:19:16 +0200

    first blad

--- conftest.py
+++ conftest.py
@@ -1,7 +1,14 @@
 import pytest
 #from helpers import generate_random_book_title
-from main import BooksCollector
+from main import *
+from data import *
+
 @pytest.fixture()
 def collector():
     collector = BooksCollector()
+    return collector
+
+@pytest.fixture()
+def collector_with_one_book(collector):
+    collector.add_new_book(BOOK_TITLE)
     return collector
--- tests/tests.py
+++ tests/tests.py
@@ -1,4 +1,8 @@
+import pytest
 from main import BooksCollector
+from data import *
+from conftest import *
+
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
@@ -8,9 +12,9 @@ class TestBooksCollector:
     # обязательно указывать префикс test_
     # дальше идет название метода, который тестируем add_new_book_
     # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
+    def test_add_new_book_add_two_books(self, collector):
         # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+        #collector = BooksCollector()
 
         # добавляем две книги
         collector.add_new_book('Гордость и предубеждение и зомби')
@@ -26,6 +30,24 @@ class TestBooksCollector:
     # проверям, что две книги с одинаковым названием добавить не получится
     def test_add_new_book_add_same_book(self, collector):
         #collector = BooksCollector()
-        collector.add_new_book('Гордость и предубеждение')
-        collector.add_new_book('Гордость и предубеждение')
+        collector.add_new_book(BOOK_TITLE)
+        collector.add_new_book(BOOK_TITLE)
         assert len(collector.books_genre) == 1
+
+    # проверяем, что книгу нелья добавть книги без названия ипи больше 40 символов
+    @pytest.mark.parametrize('book_title_wrong', WRONG_BOOK_TITLE)
+    def test_add_new_book_wrong_name(self, collector, book_title_wrong):
+        collector.add_new_book(book_title_wrong)
+        assert len(collector.books_genre) == 0
+
+    #проверям, что можно добавть жанр книги
+    @pytest.mark.parametrize('book_genre', GENRE)
+    def test_set_book_genre_from_genre(self, collector_with_one_book, book_genre):
+        collector_with_one_book.set_book_genre(BOOK_TITLE, book_genre)
+        assert collector_with_one_book.get_book_genre(BOOK_TITLE) == book_genre
+
+    #check set genre not from GENRE
+    @pytest.mark.parametrize('book_genre_wrong', WRONG_GENRE)
+    def test_set_book_genre_not_from_genre(self, collector_with_one_book, book_genre_wrong):
+        collector_with_one_book.set_book_genre(BOOK_TITLE, book_genre_wrong)
+        assert collector_with_one_book.get_book_genre(BOOK_TITLE) == ''
